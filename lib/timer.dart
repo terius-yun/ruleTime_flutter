ba8ca9e4a872df3c410b50d699b420c067b704fa
DESKTOP-C0AVHKN\bbb08 <tingtangting9398@gmail.com>
2020-10-10 20:45:44 +0900

    변경 사항 : timer.dart 파일 전체 레이아웃 expanded로 감싼 후 flex값 지정. numberpicker column으로 감싸기. 타이머 온 오프 조정을 위해 불린 몇몇의 변수 선언(아직 처리되지않음)
    문제사항 : 38번 라인 MaterialPageRoute로 이동하였을때 앱바와 하단 네비게이터가
    없어진 상태에서 불러오는 문제 발생 해당부분 확인 후 처리할것
    
    다음 목표: 문제 사항 해결 후 start,stop버튼 클릭에 따른 기능 수행이 가능하게 할것.

--- lib/timer.dart
+++ lib/timer.dart
@@ -1,3 +1,5 @@
+import 'package:alarmflutterapp/home.dart';
+import 'package:alarmflutterapp/main.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
@@ -14,21 +16,48 @@ class _TimerExState extends State<TimerEx> {
   int _min = 0;
   int _hour = 0;
   int timeForTimer = 0;
+  bool started = true;
+  bool stopped = true;
+  bool checktimer = true;
   String timeToDisplay = "";
 
   void startTimer() {
+    setState(() {
+      started = false;
+      stopped = false;
+    });
     timeForTimer = ((_hour * 60 * 60) + (_min * 60) + _sec);
     const oneSec = const Duration(seconds: 1);
     _timer = new Timer.periodic(
       oneSec,
       (Timer timer) => setState(
         () {
-          if (timeForTimer < 1) {
+          if (timeForTimer < 1 || checktimer == false) {
             timer.cancel();
+            if (timeForTimer == 0) {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MyHomePage(),
+                  ));
+            }
           } else if (timeForTimer < 60) {
             timeToDisplay = timeForTimer.toString();
             timeForTimer = timeForTimer - 1;
-          } else if (timeForTimer < 3600) {}
+          } else if (timeForTimer < 3600) {
+            int m = timeForTimer ~/ 60;
+            int s = timeForTimer - (60 * m);
+            timeToDisplay = m.toString() + ":" + s.toString();
+            timeForTimer = timeForTimer - 1;
+          } else {
+            int h = timeForTimer ~/ 3600;
+            int t = timeForTimer - (3600 * h);
+            int m = t ~/ 60;
+            int s = t - (60 * m);
+            timeToDisplay =
+                h.toString() + ":" + m.toString() + ":" + s.toString();
+            timeForTimer = timeForTimer - 1;
+          }
         },
       ),
     );
@@ -46,45 +75,127 @@ class _TimerExState extends State<TimerEx> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Center(
+          Expanded(
+            flex: 6,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                new NumberPicker.integer(
-                  initialValue: _hour,
-                  minValue: 0,
-                  maxValue: 24,
-                  onChanged: (value) => setState(() => _hour = value),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 10.0),
+                      child: Text(
+                        "HH",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                    new NumberPicker.integer(
+                      initialValue: _hour,
+                      minValue: 0,
+                      maxValue: 24,
+                      onChanged: (value) => setState(() => _hour = value),
+                    ),
+                  ],
                 ),
-                new NumberPicker.integer(
-                  initialValue: _min,
-                  minValue: 0,
-                  maxValue: 60,
-                  onChanged: (value) => setState(() => _min = value),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 10.0),
+                      child: Text(
+                        "MM",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                    new NumberPicker.integer(
+                      initialValue: _min,
+                      minValue: 0,
+                      maxValue: 60,
+                      onChanged: (value) => setState(() => _min = value),
+                    ),
+                  ],
                 ),
-                new NumberPicker.integer(
-                  initialValue: _sec,
-                  minValue: 0,
-                  maxValue: 60,
-                  onChanged: (value) => setState(() => _sec = value),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 10.0),
+                      child: Text(
+                        "SS",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                    new NumberPicker.integer(
+                      initialValue: _sec,
+                      minValue: 0,
+                      maxValue: 60,
+                      onChanged: (value) => setState(() => _sec = value),
+                    ),
+                  ],
                 ),
               ],
             ),
           ),
-          Center(
-            child: RaisedButton(
-              onPressed: () {
-                startTimer();
-              },
-              child: Text("start"),
-              color: Colors.purple,
-              textColor: Colors.white,
+          Expanded(
+            flex: 1,
+            child: Text(
+              "$timeToDisplay",
+              style: TextStyle(fontSize: 30.0),
             ),
           ),
-          Text(
-            "$timeToDisplay",
-            style: TextStyle(fontSize: 30.0),
-          )
+          Expanded(
+            flex: 3,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 40.0, vertical: 10.0),
+                  onPressed: () {
+                    startTimer();
+                  },
+                  child: Text(
+                    "start",
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  color: Colors.purple,
+                  textColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
+                SizedBox(
+                  width: 30.0,
+                ),
+                RaisedButton(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 40.0, vertical: 10.0),
+                  onPressed: () {
+                    startTimer();
+                  },
+                  child: Text(
+                    "stop",
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  color: Colors.purple,
+                  textColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
